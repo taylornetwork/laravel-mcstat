c0be0c4430cece2dd56e8a9c74fb2d0bd7937595
Sam Taylor <sam@taylornetwork.ca>
2020-04-26 09:05:19 -0400

    remove migration namespace

--- src/migrations/2020_04_26_000000_create_servers_table.php
+++ src/migrations/2020_04_26_000000_create_servers_table.php
@@ -1,9 +1,5 @@
 <?php
 
-
-namespace TaylorNetwork\LaravelMcStat\migrations;
-
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
